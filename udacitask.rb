f528a1c4edd23e1acf11994b27efb149d9944fed
Cyril Leroux <cyr.leroux@outlook.com>
2016-01-24 23:41:39 +0100

    Adding the report file creation feature

--- udacitask.rb
+++ udacitask.rb
@@ -1,14 +1,33 @@
 require_relative 'todolist.rb'
 
+# Creation of the log file (New feature)
+$log_file = File.new("report.txt", "w+")
+
+
 #initialization of parameters in order to avoid harcoded values
 nb_tasks_to_create = 4
 tasks_to_delete_list = [0,1]
 tasks_for_status_update =[0]
 
-def automatic_task_deletion(tasks_to_delete_list,my_todo_list)
+def automatic_task_deletion(tasks_to_delete_list,my_todo_list,log_file)
   tasks_to_delete_list.each do |task|
       my_todo_list.remove_an_item(task)
+      copy_to_log_file(log_file,"Task deletion",list:my_todo_list.to_do_list,id:task)
       my_todo_list.print_out_all_items
+  end
+end
+
+# New feature 1 : log file creation in order to record all the changes in to the list.
+# Record any action into a log file
+def copy_to_log_file(file,action,options = {})
+  if (options[:list] && action =="Task creation")
+    file.puts("#{Time.now}  : #{action} #{options[:list].last.description} ")
+  end
+  if (options[:list] && action =="Task deletion")
+    file.puts("#{Time.now}  : #{action} #{options[:list].at(options[:id]).description}")
+  end
+  if action =="List name update"
+    file.puts("#{Time.now}  : #{action}")
   end
 end
 
@@ -19,6 +38,7 @@ my_todo_list = TodoList.new
 puts("For initialization, we create #{nb_tasks_to_create} tasks")
 nb_tasks_to_create.times do
   my_todo_list.add_an_item
+  copy_to_log_file($log_file,"Task creation",{list:my_todo_list.to_do_list})
 end
 
 # Print the list
@@ -30,7 +50,7 @@ my_todo_list.print_out_all_items
 # Print the list
 # Delete the second item
 # Print the list
-automatic_task_deletion(tasks_to_delete_list,my_todo_list)
+automatic_task_deletion(tasks_to_delete_list,my_todo_list,$log_file)
 
 # Update the completion status of the first item to complete
 tasks_for_status_update.each do |task|
@@ -43,6 +63,7 @@ my_todo_list.print_out_all_items
 # Update the title of the list
 puts("Update of the name of the tasks list in an automatic mode")
 my_todo_list.title = "Updated title"
+copy_to_log_file($log_file,"List name update")
 
 # Print the list
 my_todo_list.print_out_all_items
