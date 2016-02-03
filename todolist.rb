becd69cdf0a76d41be65a98749e28f8ccb78f759
Cyril Leroux <cyr.leroux@outlook.com>
2016-02-03 23:31:02 +0100

    the question ?

--- todolist.rb
+++ todolist.rb
@@ -1,3 +1,6 @@
+#########################################################################
+########################### To do list ##################################
+
 class TodoList
 
     # methods and stuff go here
@@ -5,18 +8,42 @@ class TodoList
     def initialize
       @title = "My To do list"
       @to_do_list = []
+      @list_completion_status
     end
 
     # Method to add a task into the list
     def add_an_item
       item = Item.new
-      item.description = "Task number #{to_do_list.size}"
+      item.description = "Task number #{to_do_list.size+1}"
       to_do_list << item
     end
 
     # Method to remove a task from the list
     def remove_an_item(code_item_to_delete)
-      to_do_list.delete_at(code_item_to_delete)
+      to_do_list.delete_at(code_item_to_delete-1)
+    end
+
+    # Method to delete several items in once
+    def items_list_deletion(tasks_to_delete_list,log_file)
+      tasks_to_delete_list.each do |task|
+          copy_to_log_file(log_file,"Task deletion",to_do_list:to_do_list,id:task)
+          remove_an_item(task)
+          print_out_all_items
+      end
+    end
+
+    # New feature 1 : log file creation in order to record all the changes in to the list.
+    # Record any action into a log file
+    def copy_to_log_file(file,action,options = {})
+      if (options[:to_do_list] && action =="Task creation")
+        file.puts("#{Time.now}  : #{action} #{options[:to_do_list].last.description} ")
+      end
+      if (options[:to_do_list] && action =="Task deletion")
+        file.puts("#{Time.now}  : #{action} #{options[:to_do_list].at(options[:id]).description}")
+      end
+      if action =="List name update"
+        file.puts("#{Time.now}  : #{action}")
+      end
     end
 
     # Method to update the completion status of a task into the list
@@ -40,19 +67,28 @@ class TodoList
         item.print_details
       end
     end
+
+    #New feature 3 if all the tasks of the list are completed then
+    #the task list is completed
+    def to_do_list_completed?
+      return @to_do_list.all? {|item| item.completion_status == true}
+    end
+
+
 end
 
-class Item
-    attr_accessor :description, :completion_status, :beg_date, :end_date,
-    :completion_status, :owner
+#########################################################################
+########################### Item ########################################
 
+class Item
+    attr_accessor :description, :completion_status
     def initialize
       @description = description
       @beg_date = Time.now
       @end_date = Time.now + (24*60 *60)
       @completion_status = false
       @completion_level = level_completion_mana(@beg_date,@end_date)
-      @owner = "Not allocated"
+      @owner = ""
     end
 
     def update_status
@@ -61,7 +97,7 @@ class Item
 
     def print_details
       puts ("------------------------------------------------------------------------------")
-      puts ("|#{@description}     end date  :#{@end_date} |#{completion_status_management} | level of completion #{level_completion_mana(@beg_date,@end_date)} % | owner : #{@owner}")
+      puts ("|#{@description}     end date  :#{@end_date} |#{completion_status_management} | level of completion #{level_completion_mana(@beg_date,@end_date)} %")
       puts ("------------------------------------------------------------------------------")
     end
 
@@ -77,4 +113,8 @@ class Item
     def level_completion_mana(beg_date,end_date)
       return ((Time.now - beg_date)/(end_date - beg_date)*100).round(2)
     end
+
 end
+
+################################################################################
+########################### End of file ########################################
--- udacitask.rb
+++ udacitask.rb
@@ -1,85 +1,51 @@
+########################### Gem import #########################################
 require_relative 'todolist.rb'
-require 'colorize'
+########################### Initialization #####################################
 
 # Creation of the log file (New feature)
 $log_file = File.new("report.txt", "w+")
 
-
-#initialization of parameters in order to avoid harcoded values and allow an automatic
-#execution at the begining
+# Initialization of parameters in order to avoid harcoded values
+# and allow an automatic execution at the begining
 nb_tasks_to_create = 4
 tasks_to_delete_list = [0,1]
 tasks_for_status_update =[0]
 
-def automatic_task_deletion(tasks_to_delete_list,my_todo_list,log_file)
-  tasks_to_delete_list.each do |task|
-      copy_to_log_file(log_file,"Task deletion",list:my_todo_list.to_do_list,id:task)
-      my_todo_list.remove_an_item(task)
-      ##tasks_to_delete_list.delete_at(task)
-      my_todo_list.print_out_all_items
-  end
-end
-
-# New feature 1 : log file creation in order to record all the changes in to the list.
-# Record any action into a log file
-def copy_to_log_file(file,action,options = {})
-  if (options[:list] && action =="Task creation")
-    file.puts("#{Time.now}  : #{action} #{options[:list].last.description} ")
-  end
-  if (options[:list] && action =="Task deletion")
-    file.puts("#{Time.now}  : #{action} #{options[:list].at(options[:id]).description}")
-  end
-  if action =="List name update"
-    file.puts("#{Time.now}  : #{action}")
-  end
-end
-
-
-
-
-#def color_selection(level_completion_mana(beg_date, end_date))
-
-#end
-
-#def colorized_console_message(color_selection(level_completion_mana(beg_date, end_date)),task_label)
-
-#end
-
-
 # Creates a new todo list
 my_todo_list = TodoList.new
+
+########################### Main programm ######################################
 
 # Add the number of tasks to create
 puts("For initialization, we create #{nb_tasks_to_create} tasks")
 nb_tasks_to_create.times do
   my_todo_list.add_an_item
-  copy_to_log_file($log_file,"Task creation",{list:my_todo_list.to_do_list})
+  my_todo_list.copy_to_log_file($log_file,"Task creation",{to_do_list:my_todo_list.to_do_list})
 end
 
 # Print the list
 my_todo_list.print_out_all_items
 
-# Remark : the following requirements are covered by the call
-#           of the function automatic_task_deletion
-
 # Delete the first item
 # Print the list
 # Delete the second item
 # Print the list
-automatic_task_deletion(tasks_to_delete_list,my_todo_list,$log_file)
+my_todo_list.items_list_deletion(tasks_to_delete_list,$log_file)
 
 # Update the completion status of the first item to complete
 tasks_for_status_update.each do |task|
   my_todo_list.update_an_item(task)
 end
 
-# Print the list
 my_todo_list.print_out_all_items
 
 # Update the title of the list
 puts("Update of the name of the tasks list in an automatic mode")
 my_todo_list.title = "Updated title"
-copy_to_log_file($log_file,"List name update")
+my_todo_list.copy_to_log_file($log_file,"List name update")
 
 # Print the list
 my_todo_list.print_out_all_items
+
+################################################################################
+########################### End of file ########################################
